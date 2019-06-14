e6913fd5af5e17b36598d8ebb4483aa32c1ac326
rjurado01 <rjurado01@gmail.com>
2019-06-14 21:46:00 +0200

    Added simple authentication

--- lib/my_app_web/views/user_view.ex
+++ lib/my_app_web/views/user_view.ex
@@ -15,4 +15,13 @@ defmodule MyAppWeb.UserView do
       email: user.email,
       is_active: user.is_active}
   end
+
+  def render("sign_in.json", %{user: user}) do
+    %{
+      data: %{
+        id: user.id,
+        email: user.email
+      }
+    }
+  end
 end
