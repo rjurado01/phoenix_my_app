2e8d77ef63461660e372bc544468bc6ff5aa6e75
rjurado01 <rjurado01@gmail.com>
2019-09-10 17:43:45 +0200

    wip

---
+++ aux.rb
@@ -0,0 +1,16 @@
+a b c d e
+1 2 3 4 5
+v w x y z
+
+b 2 w 5
+b 2 w 6
+b 2 x 7
+b 3 x 8
+b 3 z 9
+c 1 v 1
+c 2 v 2
+c 3 y 4
+
+-> b 2 w 6
+
+A > b or (A = b and B > 2)
--- lib/app/queries/querie.ex
+++ lib/app/queries/querie.ex
@@ -5,13 +5,14 @@ defmodule App.Query do
       import Ecto.Query.API, only: [field: 2]
 
       def filter(query, params) do
-        Enum.reduce params, query, fn x, acc ->
+        Enum.reduce(params, query, fn x, acc ->
           try do
             filter_by(acc, x)
-          rescue ArgumentError ->
-            query
+          rescue
+            ArgumentError ->
+              query
           end
-        end
+        end)
       end
 
       def order(query, params) do
@@ -20,13 +21,14 @@ defmodule App.Query do
         if order_info do
           fields = String.split(order_info, ",")
 
-          order_params = Enum.reduce fields, [], fn x, acc ->
-            matches = Regex.run(~r/(\w+)(-?)/, x)
-            field = Enum.at(matches, 1)
-            dir = if Enum.at(matches, 2) == "-", do: :desc, else: :asc
+          order_params =
+            Enum.reduce(fields, [], fn x, acc ->
+              matches = Regex.run(~r/(\w+)(-?)/, x)
+              field = Enum.at(matches, 1)
+              dir = if Enum.at(matches, 2) == "-", do: :desc, else: :asc
 
-            acc ++ [{dir, String.to_atom(field)}]
-          end
+              acc ++ [{dir, String.to_atom(field)}]
+            end)
 
           order_by(query, ^order_params)
         else
@@ -43,14 +45,11 @@ defmodule App.Query do
       #
       # field = :email
       # App.User |> Ecto.Query.where([x], field(x, ^field) == "user2@email.com") |> App.Repo.all
-      def paginate(query \\ App.User) do
+      def paginate(query \\ App.User, limit \\ 20, order_params) do
         order_field = :is_admin
         order_value = false
         id_value = 5
         limit_value = 2
-
-        # query = Ecto.Query.dynamic([x], x.email == "iiiiii")
-        # App.User |> Ecto.Query.where([x], ^query) |> App.Repo.all
 
         cond do
           order_field and id_value ->
@@ -59,16 +58,68 @@ defmodule App.Query do
               [x],
               field(x, ^order_field) >= ^order_value and
                 (field(x, ^order_field) != ^order_value or x.id > ^id_value)
-            ) |> limit(^limit_value)
+            )
+            |> limit(^limit_value)
+
           id_value ->
             where(query, [x], x.id > ^id_value) |> limit(^limit_value)
-          true
+            true
             query |> limit(^limit_value)
         end
       end
 
-      def example(x) do
-        field(x, "value")
+      defmacro custom_where(query, field, value, operator) do
+        result = {
+          operator,
+          [context: Elixir, import: Kernel],
+          [
+            {:field, [], [query, {:^, [], [field]}]},
+            {:^, [], [value]}
+          ]
+        }
+
+        IO.inspect(Macro.to_string(result))
+
+        result
+      end
+
+      defmacro m1(x) do
+        {
+          :==,
+          [context: Elixir, import: Kernel],
+          [
+            {:field, [], [x, :is_admin]},
+            true
+          ]
+        }
+      end
+
+      defmacro build_query(x) do
+        conditions = Enum.map([true, false], fn v ->
+          {
+            :==,
+            [context: Elixir, import: Kernel],
+            [
+              {:field, [], [x, :is_admin]},
+              v
+            ]
+          }
+        end)
+
+        {
+          :and,
+          [context: Elixir, import: Kernel],
+          conditions
+        }
+      end
+
+      def example do
+        Ecto.Query.where(
+          App.User,
+          [x],
+          build_query(x)
+          # custom_where(x, :email, "some@email.com", :==) and custom_where(x, :is_admin, true, :==)
+        )
       end
     end
   end
